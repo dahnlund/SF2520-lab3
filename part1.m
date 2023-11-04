37440943d19509e6292986b3009aaa6adbbf045f
Emil G <eim.wco@gmail.com>
2023-11-04 18:38:27 +0100

    cleanup

--- part1.m
+++ part1.m
@@ -1,5 +1,5 @@
 % Computer exercise 3 Part 1, David Ahnlund Emil Gestsson
-clc, clear;
+clc, clear; global u0_value d;
 
 N = 100; %Discretization resolution in x
 
@@ -15,17 +15,14 @@ M = d*2*T*N^2/(Lx^2);  %Discretization resolution in t (according to theory)
 dx = Lx/N;
 dt = T/M;
 
+% dirichlet condition at u(0, t)
+u0_value= @(t) sin(pi*t/a) .* (t<=a);
+
+% add values at the boundaries
+add_bounds = @(t, u) [u0_value(t); u; (4*u(end, :)-u(end-1, :)) / 3];
+
 %% A
-A = d*1/dx^2 * spdiags([1*ones(N-1,1) (-2*ones(N-1,1)) 1*ones(N-1,1)], -1:1, N-1, N-1);
-
-%Adjust for Neumann boundary condition
-A(end,end) = d*1/dx^2 * (-2/3);
-A(end,end-1) = d*1/dx^2 * (2/3);
-
-b = @(t) d/(dx^2)*[ sin(pi*t/a) * (t<=a) ;zeros(N-2,1)];
-
-% Initial condition
-u0 = zeros(N-1,1);
+[u0, A, b, dudt] = create_system(N, dx);
 
 saved_u = zeros(N-1,M+1);
 saved_u(:,1) = u0;
@@ -34,15 +31,12 @@ saved_u(:,1) = u0;
 uk = u0;
 t = 0:dt:T;
 for n = 2:length(t)
-    u_new = uk + dt*(A*uk + b(t(n)));
+    u_new = uk + dt*dudt(t(n), uk);
     saved_u(:,n) = u_new;
     uk = u_new;
 end
 
-%Apply boundaries to solution
-uN = 1/3*(4*saved_u(end,:)-saved_u(end-1,:));
-u_initial = sin(pi*t/a) .* (t<=a);
-saved_u = [u_initial;saved_u;uN];
+saved_u = add_bounds(t, saved_u);
 
 x = 0:dx:Lx;
 surf(t,x,saved_u)
@@ -83,31 +77,17 @@ for i = 1:length(N_list)
     N = N_list(i);
     dx = Lx/N;
     
-    u0 = zeros(N-1,1);
-
-    A = d*1/dx^2 * spdiags([1*ones(N-1,1) (-2*ones(N-1,1)) 1*ones(N-1,1)], -1:1, N-1, N-1);
-
-    %Adjust for Neumann boundary condition
-    A(end,end) = d*1/dx^2 * (-2/3);
-    A(end,end-1) = d*1/dx^2 * (2/3);
-    
-    b = @(t) d/(dx^2)*[ sin(pi*t/a) * (t<=a) ;zeros(N-2,1)];
-
-    dudt = @(t,u) A*u+b(t);
+    [u0, A, b, dudt] = create_system(N, dx);
     options = odeset(RelTol=1e-4);
 
     tic;
     [t, u_ode23] = ode23(dudt, [0 2], u0, options);
     time = toc;
-    u_ode23 = u_ode23';
-    
-    %Apply boundaries to solution
-    uN = 1/3*(4*u_ode23(end,:)-u_ode23(end-1,:));
-    u_initial = (sin(pi*t/a) .* (t<=a))';
-    u_ode23 = [u_initial;u_ode23;uN];
+    u_ode23 = add_bounds(t', u_ode23');
+
     fprintf("ODE23: #Time steps = %.0d, for N = %.0d, CPU-time: %.08f seconds\n", length(t), N, time)
     
-    if N == 100
+    if i == 1
         x = 0:Lx/N:Lx;
         surf(t,x,u_ode23)
         shading interp
@@ -122,31 +102,18 @@ for i = 1:length(N_list)
     N = N_list(i);
     dx = Lx/N;
     
-    u0 = zeros(N-1,1);
-
-    A = d*1/dx^2 * spdiags([1*ones(N-1,1) (-2*ones(N-1,1)) 1*ones(N-1,1)], -1:1, N-1, N-1);
-
-    %Adjust for Neumann boundary condition
-    A(end,end) = d*1/dx^2 * (-2/3);
-    A(end,end-1) = d*1/dx^2 * (2/3);
-    
-    b = @(t) d/(dx^2)*[ sin(pi*t/a) * (t<=a) ;zeros(N-2,1)];
-
-    dudt = @(t,u) A*u+b(t);
+    [u0, A, b, dudt] = create_system(N, dx);
     options = odeset(RelTol=1e-4);
 
     tic;
     [t, u_ode23s] = ode23s(dudt, [0 2], u0, options);
     time = toc;
-    u_ode23s = u_ode23s';
-    
-    %Apply boundaries to solution
-    uN = 1/3*(4*u_ode23s(end,:)-u_ode23s(end-1,:));
-    u_initial = (sin(pi*t/a) .* (t<=a))';
-    u_ode23s = [u_initial;u_ode23s;uN];
+
+    u_ode23s = add_bounds(t', u_ode23s');
+
     fprintf("ODE23s: #Time steps = %.0d, for N = %.0d, CPU-time: %.08f seconds\n", length(t), N, time)
     
-    if N == 100
+    if i == 1
         x = 0:Lx/N:Lx;
         surf(t,x,u_ode23s)
         shading interp
@@ -161,34 +128,36 @@ for i = 1:length(N_list)
     N = N_list(i);
     dx = Lx/N;
     
-    u0 = zeros(N-1,1);
-
-    A = d*1/dx^2 * spdiags([1*ones(N-1,1) (-2*ones(N-1,1)) 1*ones(N-1,1)], -1:1, N-1, N-1);
-
-    %Adjust for Neumann boundary condition
-    A(end,end) = d*1/dx^2 * (-2/3);
-    A(end,end-1) = d*1/dx^2 * (2/3);
-    
-    b = @(t) d/(dx^2)*[ sin(pi*t/a) * (t<=a) ;zeros(N-2,1)];
-
-    dudt = @(t,u) A*u+b(t);
+    [u0, A, b, dudt] = create_system(N, dx);
     options = odeset("Jacobian",A,RelTol=1e-4);
     
     tic;
     [t, u_ode23sJ] = ode23s(dudt, [0 2], u0, options);
     time = toc;
-    u_ode23sJ = u_ode23sJ';
     
-    %Apply boundaries to solution
-    uN = 1/3*(4*u_ode23sJ(end,:)-u_ode23sJ(end-1,:));
-    u_initial = (sin(pi*t/a) .* (t<=a))';
-    u_ode23sJ = [u_initial;u_ode23sJ;uN];
+    u_ode23sJ = add_bounds(t', u_ode23sJ');
+
     fprintf("ODE23sJ: #Time steps = %.0d, for N = %.0d, CPU-time: %.08f seconds\n", length(t), N, time)
     
-    if N == 100
+    if i == 1
         x = 0:Lx/N:Lx;
         surf(t,x,u_ode23sJ)
         shading interp
     end
 end
 fprintf("\n\n")
+
+%% Avoid repeat code
+function [u0, A, b, dudt] = create_system(N, dx)
+    global u0_value d;
+
+    u0 = zeros(N-1,1);
+    A = d*1/dx^2 * spdiags([1*ones(N-1,1) (-2*ones(N-1,1)) 1*ones(N-1,1)], -1:1, N-1, N-1);
+
+    %Adjust for Neumann boundary condition
+    A(end,end) = d*1/dx^2 * (-2/3);
+    A(end,end-1) = d*1/dx^2 * (2/3);
+    
+    b = @(t) d/(dx^2)*[ u0_value(t); zeros(N-2,1)];
+    dudt = @(t,u) A*u+b(t);
+end
